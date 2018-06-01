c1c50c8b6f868c6a98c91ba32c1ecfeaf6bb4ec2
Mustafa Bayer <mustafa.bayer@uyumsoft.com>
2018-06-01 17:11:41 +0300

    temizlik devam

--- src/routes.js
+++ src/routes.js
@@ -17,41 +17,6 @@ const Dashboard = Loadable({
   loading: Loading,
 });
 
-const CoreUIIcons = Loadable({
-  loader: () => import('./views/Icons/CoreUIIcons'),
-  loading: Loading,
-});
-
-const Flags = Loadable({
-  loader: () => import('./views/Icons/Flags'),
-  loading: Loading,
-});
-
-const FontAwesome = Loadable({
-  loader: () => import('./views/Icons/FontAwesome'),
-  loading: Loading,
-});
-
-const SimpleLineIcons = Loadable({
-  loader: () => import('./views/Icons/SimpleLineIcons'),
-  loading: Loading,
-});
-
-const Alerts = Loadable({
-  loader: () => import('./views/Notifications/Alerts'),
-  loading: Loading,
-});
-
-const Badges = Loadable({
-  loader: () => import('./views/Notifications/Badges'),
-  loading: Loading,
-});
-
-const Modals = Loadable({
-  loader: () => import('./views/Notifications/Modals'),
-  loading: Loading,
-});
-
 const Widgets = Loadable({
   loader: () => import('./views/Widgets/Widgets'),
   loading: Loading,
@@ -68,17 +33,6 @@ const routes = [
   { path: '/', exact: true, name: 'Ana Sayfa', component: DefaultLayout },
   { path: '/dashboard', name: 'Gösterge Paneli', component: Dashboard },
 
-  { path: '/icons', exact: true, name: 'Icons', component: CoreUIIcons },
-  { path: '/icons/coreui-icons', name: 'CoreUI Icons', component: CoreUIIcons },
-  { path: '/icons/flags', name: 'Flags', component: Flags },
-  { path: '/icons/font-awesome', name: 'Font Awesome', component: FontAwesome },
-  { path: '/icons/simple-line-icons', name: 'Simple Line Icons', component: SimpleLineIcons },
-  
-  { path: '/notifications', exact: true, name: 'Notifications', component: Alerts },
-  { path: '/notifications/alerts', name: 'Alerts', component: Alerts },
-  { path: '/notifications/badges', name: 'Badges', component: Badges },
-  { path: '/notifications/modals', name: 'Modals', component: Modals },
-  
   { path: '/widgets', name: 'Widgets', component: Widgets },
   
   { path: '/charts', name: 'Charts', component: Charts },
