1bc0287f51aa52dccb7ea639a9578af0e5c31ab1
Mustafa Bayer <mustafa.bayer@uyumsoft.com>
2018-06-04 11:40:24 +0300

    login'e baslaniyor

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -1,53 +1,103 @@
 import React, { Component } from 'react';
-import { Button, Card, CardBody, CardGroup, Col, Container, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import { Button, Card, CardBody, CardGroup, Col, Container, Input, InputGroup, InputGroupAddon, InputGroupText, Modal, ModalBody, ModalFooter, ModalHeader, Row } from 'reactstrap';
+import PreloaderIcon from 'react-preloader-icon';
+import Puff from 'react-preloader-icon/loaders/Puff';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      danger: false,
+      username: '',
+      password: ''
+    };
+
+    this.onForgot = this.onForgot.bind(this);
+    this.updateUsername = this.updateUsername.bind(this);
+    this.updatePassword = this.updatePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onForgot() {
+    this.setState({
+      danger: !this.state.danger,
+    });
+  }
+
+  updateUsername(e) {
+    this.setState({username: e.target.value});
+  }
+
+  updatePassword(e) {
+    this.setState({password: e.target.value});
+  }
+
+  componentWillMount() {
+    this.setState({ username: ' ', password: ''});
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      loading: !this.state.loading,
+    });
+    console.log('not now later');
+  }
+
   render() {
     return (
       <div className="app flex-row align-items-center">
         <Container>
           <Row className="justify-content-center">
-            <Col md="8">
+            <Col md="4">
               <CardGroup>
                 <Card className="p-4">
-                  <CardBody>
-                    <h1>Login</h1>
-                    <p className="text-muted">Sign In to your account</p>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-user"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="text" placeholder="Username" />
-                    </InputGroup>
-                    <InputGroup className="mb-4">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-lock"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="password" placeholder="Password" />
-                    </InputGroup>
-                    <Row>
-                      <Col xs="6">
-                        <Button color="primary" className="px-4">Login</Button>
-                      </Col>
-                      <Col xs="6" className="text-right">
-                        <Button color="link" className="px-0">Forgot password?</Button>
-                      </Col>
-                    </Row>
-                  </CardBody>
-                </Card>
-                <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: 44 + '%' }}>
-                  <CardBody className="text-center">
-                    <div>
-                      <h2>Sign up</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
-                      <Button color="primary" className="mt-3" active>Register Now!</Button>
-                    </div>
-                  </CardBody>
+                  <form onSubmit={this.onSubmit}>
+                    <CardBody>
+                      <h1>Login</h1>
+                      <p className="text-muted">Sign In to your account</p>
+                      <InputGroup className="mb-3">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-user"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="text" value = {this.state.username} onChange = {this.updateUsername} placeholder="Username" disabled = {(this.state.loading) ? "disabled" : ""} />
+                      </InputGroup>
+                      <InputGroup className="mb-4">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-lock"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="password" value = {this.state.password} onChange = {this.updatePassword} placeholder="Password" disabled = {(this.state.loading) ? "disabled" : ""}/>
+                      </InputGroup>
+                      <Row>
+                        <Col xs="6">
+                          { this.state.loading ? 
+                            <PreloaderIcon loader={Puff} size={60} strokeWidth={8} strokeColor="#006064" duration={800} /> 
+                            :
+                            <Button color="primary" className="px-4" >Login</Button> 
+                          }
+                        </Col>
+                        <Col xs="6" className="text-right">
+                          <Button onClick = {this.onForgot} color="link" className="px-0">Forgot password?</Button>
+                          <Modal isOpen={this.state.danger} toggle={this.toggleDanger}
+                                className={'modal-danger ' + this.props.className}>
+                            <ModalHeader toggle={this.toggleDanger}>Forgot password</ModalHeader>
+                            <ModalBody>
+                              We are unable to assist you right now, please contact the authorities.
+                            </ModalBody>
+                            <ModalFooter>
+                              <Button color="danger" onClick={this.onForgot}>Ok</Button>{' '}
+                            </ModalFooter>
+                          </Modal>
+                        </Col>
+                      </Row>
+                    </CardBody>
+                  </form>
                 </Card>
               </CardGroup>
             </Col>
